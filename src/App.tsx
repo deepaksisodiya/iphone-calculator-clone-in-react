e7c2754f3fe4ad51ce8b3e48bc29cf75cee326da
Deepak Sisodiya <deepak.sisodiya@technogise.com>
2024-08-10 16:46:43 +0530

    initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,50 @@
-import Logo from './assets/react.svg';
+import './index.css';
 
 function App() {
   return (
-    <>
-      <Logo />
-      <h1>React Template</h1>
-    </>
+    <div className="iphone-calculator">
+      <div className="header">
+        <div className="time">3:32</div>
+        <div className="status-icons">
+          <img src="./../status.png" alt="Status Icons" className="status-image" />
+        </div>
+      </div>
+
+      <div className="calculator">
+        <div className="display">0</div>
+        <div className="buttons">
+          <div className="row">
+            <button className="button function">AC</button>
+            <button className="button function">+/-</button>
+            <button className="button function">%</button>
+            <button className="button operator">÷</button>
+          </div>
+          <div className="row">
+            <button className="button number">7</button>
+            <button className="button number">8</button>
+            <button className="button number">9</button>
+            <button className="button operator">×</button>
+          </div>
+          <div className="row">
+            <button className="button number">4</button>
+            <button className="button number">5</button>
+            <button className="button number">6</button>
+            <button className="button operator">−</button>
+          </div>
+          <div className="row">
+            <button className="button number">1</button>
+            <button className="button number">2</button>
+            <button className="button number">3</button>
+            <button className="button operator">+</button>
+          </div>
+          <div className="row">
+            <button className="button number zero">0</button>
+            <button className="button number">.</button>
+            <button className="button operator">=</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
